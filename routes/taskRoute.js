a82d859a735476c5bbd029dbecdccdfa28dea20e
alfonsusrr <alfonsus737@gmail.com>
2023-07-07 19:25:39 +0800

    Fix typescripts

---
+++ routes/taskRoute.js
@@ -0,0 +1,9 @@
+"use strict";
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/taskController');
+//import task CRUD operation from controllers
+const { getTasks, setTasks, updateTask, deleteTask, } = require('../controllers/taskController');
+router.route('/').get(getTasks).post(setTasks);
+router.route('/:id').put(updateTask).delete(deleteTask);
+module.exports = router;
--- server.js
+++ server.js
@@ -5,39 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const { default: dbConnect } = require("./db/mongoose");
-const { errorHandler } = require("./middleware/errorMidleware");
+const routes_1 = require("./routes");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 // app.get('/', (req: Request, res: Response) => {
 //   res.send('Express + TypeScript Server');
 // });
 // app.get('/task', (req: Request, res: Response) => {
 //   dbConnect();
 // })
-/*
-    app.use('/', routes_1.routes);
-    app.listen(port, () => {
-        console.log(`⚡️[server]: Server started on ${port}`);
-    });
-*/
-
-//establist connection to database
-const dbConnect_1 = __importDefault(require("./db/mongoose"));
-dbConnect();
-
-
-
-//use json and urlencoded
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: false }));
-const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//use task route
-app.use('/api/tasks', require('./routes/taskRoute'));
-app.use(errorHandler);
-
-//listen to port
-app.listen(port, () => {console.log(`⚡️[server]: Server started on ${port}`);});
+app.use('/', routes_1.routes);
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
